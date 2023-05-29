47ccc1cc33f28208a01af32f74e16eb412106b14
kauanvoltz <kauan.voltz@gmail.com>
2023-05-29 14:53:19 -0300

    Primeiro commit

---
+++ src/test/java/empresa/quarrymen/projetoquerrymen/ProjetoquerrymenApplicationTests.java
@@ -0,0 +1,13 @@
+package empresa.quarrymen.projetoquerrymen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoquerrymenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
